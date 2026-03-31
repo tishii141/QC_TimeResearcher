20cb67b6b7f76143d63c29c82836ca1620f08907
tishii141 <titanium0528@gmail.com>
2026-03-31 22:27:52 +0900

    Update vite.config.js (#1)

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: '/QC_TimeResearcher-/',
+  base: '/QC_TimeResearcher/',
   plugins: [
     react(),
     tailwindcss(),
